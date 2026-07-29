72ad65c54475bb4dae04f1ec6d09243785253a57
Sahil Rajput <sahilrajput03@gmail.com>
2026-07-29 14:26:54 +0530

    Update.

---
+++ telegram-extension/content.js
@@ -0,0 +1,30 @@
+// Goal: Automatically open work tab when Telegram is opened.
+console.log("Page loaded!");
+
+setTimeout(() => {
+    const textLabelEl = [...document.querySelectorAll('#FoldersSidebar *')].find(el => el.textContent?.trim() === 'WORK');
+    const element = textLabelEl?.parentElement;
+    console.log(parent);
+
+
+    console.log("🚀 ~ element?", element);
+
+    if (element) {
+        const events = ['mousedown', 'mouseup', 'click'];
+
+        events.forEach(eventType => {
+            const event = new MouseEvent(eventType, {
+                view: window,
+                bubbles: true, // Crucial for apps that use event delegation
+                cancelable: true,
+                buttons: 1 // Left mouse button
+            });
+            element.dispatchEvent(event);
+        });
+    } else {
+        alert('Element not found in DOM.');
+    }
+
+
+}, 1_500);
+
